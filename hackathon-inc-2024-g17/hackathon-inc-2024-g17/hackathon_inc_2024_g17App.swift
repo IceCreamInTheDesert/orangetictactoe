4d706643ce137e9cb9bf86ca0b5ed534bff29937
indigoX234 <parthasarathy_dhanush@s2023.ssts.edu.sg>
2024-11-12 13:01:16 +0800

    base app
    regular boring tic tac toe

---
+++ hackathon-inc-2024-g17/hackathon-inc-2024-g17/hackathon_inc_2024_g17App.swift
@@ -0,0 +1,17 @@
+//
+//  hackathon_inc_2024_g17App.swift
+//  hackathon-inc-2024-g17
+//
+//  Created by Dhanush  on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct hackathon_inc_2024_g17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
